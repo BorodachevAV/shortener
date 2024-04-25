cadb4eed3434609d949ed97a113eb01e10915dd3
alborodachev <alborodachev@ozon.ru>
2024-04-25 21:31:37 +0300

    single shortener handler update

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,37 +2,39 @@ package main
 
 import (
 	"net/http"
+	"strings"
 )
 
 func shorten(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
+	if r.Method != http.MethodPost && r.Method != http.MethodGet {
 		http.Error(w, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
 		return
 	}
+	if r.Method != http.MethodPost {
+		body := "http://localhost:8080/EwHXdJfB"
 
-	body := "http://localhost:8080/EwHXdJfB"
+		w.Header().Add("Content-Type", "text/plain")
+		w.Header().Add("Host", "localhost:8080")
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(body))
+	} else if r.Method != http.MethodGet {
+		shortUrl := strings.Split(r.RequestURI, "/")
+		if len(shortUrl) > 2 {
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
 
-	w.Header().Add("Content-Type", "text/plain")
-	w.Header().Add("Host", "localhost:8080")
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(body))
-}
-func getUrl(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Only GET requests are allowed!", http.StatusMethodNotAllowed)
-		return
+		body := "Location: https://practicum.yandex.ru/"
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		w.Write([]byte(body))
 	}
-	body := "Location: https://practicum.yandex.ru/"
-
-	w.WriteHeader(http.StatusTemporaryRedirect)
-	w.Write([]byte(body))
 
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc(`/`, shorten)
-	mux.HandleFunc(`/EwHXdJfB`, getUrl)
+	//mux.HandleFunc(`/qwerty`, getUrl)
 	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
 		panic(err)
