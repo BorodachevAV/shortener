0e3101c7a6401d6b8b84126d3caffd3d3fa1827d
alborodachev <alborodachev@ozon.ru>
2024-04-26 11:34:54 +0300

    single shortener handler update

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,40 +1,63 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"math/rand"
 	"net/http"
 	"strings"
+	"time"
 )
 
-func shorten(w http.ResponseWriter, r *http.Request) {
+const Charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+func randString(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = Charset[seededRand.Intn(len(Charset))]
+	}
+	return string(b)
+}
+
+var urlsStorage = make(map[string]string)
+
+func shortener(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost && r.Method != http.MethodGet {
-		http.Error(w, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
+		http.Error(w, "Only POST or GET requests are allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	if r.Method == http.MethodGet {
-		body := "http://localhost:8080/EwHXdJfB"
 
+	if r.Method == http.MethodPost {
+
+		shortUrl := randString(8)
+		urlFromRequest, _ := io.ReadAll(r.Body)
+		urlsStorage[shortUrl] = string(urlFromRequest)
+		body := fmt.Sprintf("http://localhost:8080/%s", shortUrl)
 		w.Header().Add("Content-Type", "text/plain")
 		w.Header().Add("Host", "localhost:8080")
 		w.WriteHeader(http.StatusCreated)
 		w.Write([]byte(body))
 	}
 
-	if r.Method == http.MethodPost {
+	if r.Method == http.MethodGet {
 		shortUrl := strings.Split(r.RequestURI, "/")
 		if len(shortUrl) != 2 || shortUrl[1] == "" {
-			http.Error(w, "Bad Request", http.StatusBadRequest)
+			http.Error(w, "Bad Request, not short url", http.StatusBadRequest)
 			return
 		}
-		body := "Location: https://practicum.yandex.ru/"
+		w.Header().Add("Location", urlsStorage[shortUrl[1]])
 		w.WriteHeader(http.StatusTemporaryRedirect)
-		w.Write([]byte(body))
 	}
 
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, shorten)
+	mux.HandleFunc(`/`, shortener)
 	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
 		panic(err)
