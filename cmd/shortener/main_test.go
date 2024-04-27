50d771a1edce2ad7c8722c807e9a86bfb74f3b65
alborodachev <alborodachev@ozon.ru>
2024-04-27 13:31:27 +0300

    shortener tests

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -27,17 +29,30 @@ func TestShortener(t *testing.T) {
 		assert.Equal(t, 404, res.StatusCode)
 	})
 
-	//t.Run("Get 307", func(t *testing.T) {
-	//	request := httptest.NewRequest(http.MethodGet, "/qwerty", nil)
-	//	w := httptest.NewRecorder()
-	//	shortener(w, request)
-	//
-	//	res := w.Result()
-	//	// проверяем код ответа
-	//	assert.Equal(t, 307, res.StatusCode)
-	//})
+	t.Run("Get 307", func(t *testing.T) {
+		url := `https://stackoverflow.com/`
+		bodyReader := strings.NewReader(url)
+		request := httptest.NewRequest(http.MethodPost, "/", bodyReader)
+		w := httptest.NewRecorder()
+		shortener(w, request)
+		shortUrl := w.Result().Body
 
-	t.Run("Post positive", func(t *testing.T) {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(shortUrl)
+		respBytes := buf.String()
+		respBytes = respBytes[len(respBytes)-8:]
+
+		request = httptest.NewRequest(http.MethodGet, fmt.Sprintf("/%s", respBytes), nil)
+		w = httptest.NewRecorder()
+		shortener(w, request)
+
+		res := w.Result()
+
+		// проверяем код ответа
+		assert.Equal(t, url, res.Header["Location"][0])
+	})
+
+	t.Run("Post 201", func(t *testing.T) {
 		bodyReader := strings.NewReader(`https://stackoverflow.com/`)
 		request := httptest.NewRequest(http.MethodPost, "/", bodyReader)
 		w := httptest.NewRecorder()
