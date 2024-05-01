4d5c2ad0ec9445ecc3b319ba1274e3f0e0f3fceb
alborodachev <alborodachev@ozon.ru>
2024-05-01 13:38:37 +0300

    shortener on chi

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"io"
+	"log"
 	"math/rand"
 	"net/http"
-	"strings"
 	"time"
 )
 
@@ -28,59 +29,53 @@ func randString(length int) string {
 // создаем хранилище url
 var urlsStorage = make(map[string]string)
 
-// хендлер запросов
-func shortener(w http.ResponseWriter, r *http.Request) {
-	//проверка метода, обрабатываем только GET и POST
-	if r.Method != http.MethodPost && r.Method != http.MethodGet {
-		http.Error(w, "Only POST or GET requests are allowed", http.StatusMethodNotAllowed)
+func shorten(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST requests are allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	//обработка POST
-	if r.Method == http.MethodPost {
-		//читаем тело
-		urlFromRequest, _ := io.ReadAll(r.Body)
-		//генерим короткий url
-		shortUrl := randString(8)
-		// сохраняем в мапе
-		urlsStorage[shortUrl] = string(urlFromRequest)
-		//заполняем ответ
-		body := fmt.Sprintf("http://localhost:8080/%s", shortUrl)
-		w.Header().Add("Content-Type", "text/plain")
-		w.Header().Add("Host", "localhost:8080")
-		w.WriteHeader(http.StatusCreated)
-		_, err := w.Write([]byte(body))
-		if err != nil {
-			panic(err)
-		}
-	}
-	//обработка GET
-	if r.Method == http.MethodGet {
-		//делим url на части
-		shortUrl := strings.Split(r.RequestURI, "/")
-		//если адрес не сходится с ожидаемым форматом отдаем 400
-		if len(shortUrl) != 2 || shortUrl[1] == "" {
-			http.Error(w, "Bad Request, not short url", http.StatusBadRequest)
-			return
-		}
-		//проверяем в мапе наличие ключа, отдаем 404 если его нет
-		val, ok := urlsStorage[shortUrl[1]]
-		if ok {
-			w.Header().Add("Location", val)
-		} else {
-			http.Error(w, "short url not found", http.StatusNotFound)
-			return
-		}
-		//редиректим на полный адрес из мапы
-		w.WriteHeader(http.StatusTemporaryRedirect)
-	}
-
-}
-
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, shortener)
-	err := http.ListenAndServe(`:8080`, mux)
+	//читаем тело
+	urlFromRequest, _ := io.ReadAll(r.Body)
+	//генерим короткий url
+	shortUrl := randString(8)
+	// сохраняем в мапе
+	urlsStorage[shortUrl] = string(urlFromRequest)
+	//заполняем ответ
+	body := fmt.Sprintf("http://localhost:8080/%s", shortUrl)
+	w.Header().Add("Content-Type", "text/plain")
+	w.Header().Add("Host", "localhost:8080")
+	w.WriteHeader(http.StatusCreated)
+	_, err := w.Write([]byte(body))
 	if err != nil {
 		panic(err)
 	}
+}
+func expand(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Only GET requests are allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	shortUrl := chi.URLParam(r, "id")
+	//если адрес не сходится с ожидаемым форматом отдаем 400
+
+	//проверяем в мапе наличие ключа, отдаем 404 если его нет
+	val, ok := urlsStorage[shortUrl]
+	if ok {
+		w.Header().Add("Location", val)
+	} else {
+		http.Error(w, "short url not found", http.StatusNotFound)
+		return
+	}
+	//редиректим на полный адрес из мапы
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+func main() {
+	r := chi.NewRouter()
+	r.Post(`/`, shorten)
+	r.Get(`/{id}`, expand)
+	//mux := http.NewServeMux()
+	//mux.HandleFunc(`/`, shortener)
+	log.Fatal(http.ListenAndServe(":8080", r))
+
 }
