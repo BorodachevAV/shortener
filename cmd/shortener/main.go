cb4cac76c76767be8895a33b9ee9b9db75fba4f7
alborodachev <alborodachev@ozon.ru>
2024-05-27 00:21:02 +0300

    iter7

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/BorodachevAV/shortener/internal/config"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 	"io"
 	"log"
 	"math/rand"
@@ -23,6 +26,54 @@ var (
 	seededRand  *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
 	cfg                    = config.New()
 )
+
+type ShortenJsonRequest struct {
+	URL string
+}
+
+type ShortenJsonRResponse struct {
+	result string
+}
+
+func WithLogging(h http.Handler) http.Handler {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		// вызываем панику, если ошибка
+		panic(err)
+	}
+	defer logger.Sync()
+
+	// делаем регистратор SugaredLogger
+	var sugar = *logger.Sugar()
+
+	logFn := func(w http.ResponseWriter, r *http.Request) {
+		// функция Now() возвращает текущее время
+		start := time.Now()
+
+		// эндпоинт /ping
+		uri := r.RequestURI
+		// метод запроса
+		method := r.Method
+
+		// точка, где выполняется хендлер pingHandler
+		h.ServeHTTP(w, r) // обслуживание оригинального запроса
+
+		// Since возвращает разницу во времени между start
+		// и моментом вызова Since. Таким образом можно посчитать
+		// время выполнения запроса.
+		duration := time.Since(start)
+
+		// отправляем сведения о запросе в zap
+		sugar.Infoln(
+			"uri", uri,
+			"method", method,
+			"duration", duration,
+		)
+
+	}
+	// возвращаем функционально расширенный хендлер
+	return http.HandlerFunc(logFn)
+}
 
 // генерим короткий url
 func randString(length int) string {
@@ -58,6 +109,45 @@ func shorten(w http.ResponseWriter, r *http.Request) {
 		log.Println(err.Error())
 	}
 }
+
+func shortenJson(w http.ResponseWriter, r *http.Request) {
+	var req ShortenJsonRequest
+	var buf bytes.Buffer
+	// читаем тело запроса
+
+	_, err := buf.ReadFrom(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	json.Unmarshal(buf.Bytes(), &req)
+
+	_, err = url.Parse(req.URL)
+	if err != nil {
+		http.Error(w, "not url", http.StatusBadRequest)
+		return
+	}
+
+	//генерим короткий url
+	shortURL := randString(8)
+	// сохраняем в мапе
+
+	urlsStorage.Store(shortURL, req.URL)
+
+	//заполняем ответ
+	body := fmt.Sprintf("%s/%s", cfg.Cfg.BaseURL, shortURL)
+	m := make(map[string]string)
+	m["response"] = body
+	respBody, _ := json.Marshal(m)
+	w.Header().Add("Content-Type", "application/json")
+	w.Header().Add("Host", cfg.Cfg.ServerAddress)
+	w.WriteHeader(http.StatusCreated)
+	_, err = w.Write(respBody)
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
 func expand(w http.ResponseWriter, r *http.Request) {
 	shortURL := chi.URLParam(r, "id")
 	//проверяем в мапе наличие ключа, отдаем 404 если его нет
@@ -72,6 +162,7 @@ func expand(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func main() {
+
 	a := flag.String("a", "localhost:8080", "shortener host")
 	b := flag.String("b", "http://localhost:8080", "response host")
 	flag.Parse()
@@ -82,7 +173,9 @@ func main() {
 		cfg.Cfg.BaseURL = *b
 	}
 	r := chi.NewRouter()
+	r.Use(WithLogging)
 	r.Post(`/`, shorten)
+	r.Post(`/api/shorten`, shortenJson)
 	r.Get(`/{id}`, expand)
 	log.Fatal(http.ListenAndServe(cfg.Cfg.ServerAddress, r))
 }
